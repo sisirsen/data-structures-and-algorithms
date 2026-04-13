ea3cf76df44f96787af9a1c9f642686a10c42a85
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-13 11:01:01 +0530

    Print subarrays in java

---
+++ Java/Normal Question Basics/PrintSubarrays.java
@@ -0,0 +1,20 @@
+public class PrintSubarrays {
+  public static void PrintSubarrays(int arr[]){
+    int ts=0;
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i; j < arr.length-1; j++) {
+        for (int k = arr[i]; k <=arr[j]; k++) {
+          System.out.print(arr[k]+" ");
+        }
+        ts++;
+        System.out.println();
+      }
+        System.out.println();
+    }
+  System.out.println("Total subarrays : " + ts);
+  } 
+  public static void main(String[] args) {
+    PrintSubarrays(new int[]{1,2,3,4,5,6,7,8,9,10});
+  }
+}

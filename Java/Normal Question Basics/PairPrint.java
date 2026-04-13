df7c72896d20f4e3c446d0b5d5ee70ddf9d02f96
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-13 10:30:49 +0530

    fixed code of pair printing

--- Java/Normal Question Basics/PairPrint.java
+++ Java/Normal Question Basics/PairPrint.java
@@ -1,8 +1,8 @@
 public class PairPrint {
 
   public static void PairPrint (int arr[]){
-    for (int i = 0; i < arr.length-1; i++) {
-      for(int j=1;j<arr.length-1;j++){
+    for (int i = 0; i < arr.length; i++) {
+      for(int j=i+1;j<arr.length;j++){
 
         System.out.print("("+ arr[i] +","+ arr[j]+")");
       }

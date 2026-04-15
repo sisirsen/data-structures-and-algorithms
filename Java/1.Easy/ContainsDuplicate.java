cea668c03c91eb7a2ca27ac09ab52eee3316f931
SISIR SEN <sisirsen22122004@gmail.com>
2026-04-15 21:38:47 +0530

    Contailns duplicate updated code with low complexity

--- Java/1.Easy/ContainsDuplicate.java
+++ Java/1.Easy/ContainsDuplicate.java
@@ -1,53 +1,59 @@
+// import java.util.Arrays;
+// public class ContainsDuplicate {
 
-/* 
-Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+//   public static Boolean ConDup(int arr[]){
 
-Example 1:
+//     Arrays.sort(arr);
 
-Input: nums = [1,2,3,1]
+//     if (arr.length==0 || arr==null) {
+//       throw new IllegalArgumentException("Array is empty");
+//     }
 
-Output: true
+//     for (int i = 0; i < arr.length; i++) {
+//       for (int j = i+1; j < arr.length; j++) {
+//         if (arr[i]==arr[j]) {
+//           return true;
+//         }
+//       }
+//     }
+//     return false;
 
-Explanation:
 
-The element 1 occurs at the indices 0 and 3.
+//   }
 
-Example 2:
+//   public static void main(String[] args) {
+//     System.out.println(ConDup(new int[]{1,2,3,1}));
+//     System.out.println(ConDup(new int[]{1,2,3,4}));
+//     System.out.println(ConDup(new int[]{1,1,1,3,3,4,3,2,4,2}));
+//     System.out.println(ConDup(new int[]{}));
+//   }
+// }
 
-Input: nums = [1,2,3,4]
-
-Output: false
-
-Explanation:
-
-All elements are distinct.
-
-Example 3:
-
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-
-Output: true
-*/
 
 
 import java.util.Arrays;
-
 public class ContainsDuplicate {
 
-  public static boolean containsDuplicate(int[] nums){
-    Arrays.sort(nums);
-    for (int j = 0; j < nums.length-1; j++) {
-      if (nums[j]==nums[j+1]) {
-        return true;
-      }
+  public static Boolean ConDup(int arr[]){
+
+    Arrays.sort(arr);
+
+    if (arr==null|| arr.length==0 ) {
+      throw new IllegalArgumentException("Array is empty");
     }
-    return false;
 
-
+for (int i = 0; i < arr.length-1; i++) {
+  if (arr[i]==arr[i+1]) {
+    return true;
   }
+}
+return false;
+  }
+
   public static void main(String[] args) {
-    System.out.println(containsDuplicate(new int[]{1,2,3,1}));
-    System.out.println(containsDuplicate(new int[]{1,2,3,4}));
-    System.out.println(containsDuplicate(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    System.out.println(ConDup(new int[]{1,2,3,1}));
+    System.out.println(ConDup(new int[]{1,2,3,4}));
+    System.out.println(ConDup(new int[]{1,1,1,3,3,4,3,2,4,2}));
+    System.out.println(ConDup(new int[]{}));
   }
 }

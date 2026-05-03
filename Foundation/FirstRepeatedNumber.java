67d821c9c2e53751e736ba9ffd07e7cede1cd406
SISIR SEN <sisirsen22122004@gmail.com>
2026-05-03 10:30:00 +0530

    Find first repeated number of a given array if not have any repeated number then return -1

---
+++ Foundation/FirstRepeatedNumber.java
@@ -0,0 +1,20 @@
+public class FirstRepeatedNumber {
+
+  static int FirstRepeatNo(int arr[]){
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i+1; j < arr.length; j++) {
+        if (arr[i]==arr[j]) {
+        return arr[i];
+        }
+      }
+    }
+    return -1;
+
+  }
+  public static void main(String[] args) {
+    System.out.println(FirstRepeatNo(new int[]{1,2,3,1,4,5,3}));
+    System.out.println(FirstRepeatNo(new int[]{1,2,3,5,4,5,3}));
+    System.out.println(FirstRepeatNo(new int[]{1,2,3,4,5,6,7,8}));
+  }
+}

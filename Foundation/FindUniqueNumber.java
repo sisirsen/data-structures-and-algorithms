3434d0675d934f805479355720c33cad5cf59f07
SISIR SEN <sisirsen22122004@gmail.com>
2026-05-03 10:30:00 +0530

    Find a unique number from a array which was not repeated

---
+++ Foundation/FindUniqueNumber.java
@@ -0,0 +1,25 @@
+public class FindUniqueNumber {
+
+  static int findNum(int arr[]){
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i+1; j < arr.length; j++) {
+        if (arr[i]==arr[j]) {
+          arr[i]=-1;
+          arr[j]=-1;
+        }
+      }
+    }
+    int ans=-1;
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i]>ans) {
+        ans=arr[i];
+      }
+    }
+    return ans;
+
+  }
+  public static void main(String[] args) {
+    System.out.println(findNum(new int[]{1,2,3,4,2,3,1}));
+  }
+}
